f0436be8f8b4829de10d894d9c5adc9aab316082
gs202 <sheklerg@gmail.com>
2025-08-22 15:37:07 +0300

    revert conftests deletion

---
+++ tests/async/llm/conftest.py
@@ -0,0 +1,32 @@
+from typing import Iterator
+
+import pytest
+
+from llemon import (
+    LLM,
+    Anthropic,
+    DeepInfra,
+    Gemini,
+    LLMModel,
+    OpenAI,
+    errors,
+)
+
+
+@pytest.fixture(
+    params=[
+        pytest.param((OpenAI, "gpt-5-nano"), id="gpt-5-nano"),
+        pytest.param((Anthropic, "claude-3-haiku-20240307"), id="claude-3-haiku-3"),
+        pytest.param((Gemini, "gemini-2.0-flash-lite"), id="gemini-2.0-flash-lite"),
+        pytest.param((DeepInfra, "meta-llama/Meta-Llama-3.1-8B-Instruct"), id="deepinfra-llama-3.1-8b"),
+    ]
+)
+def model(request: pytest.FixtureRequest) -> Iterator[LLMModel]:
+    provider: LLM = request.param[0]
+    model: str = request.param[1]
+    try:
+        yield provider.model(model)
+    except errors.ConfigurationError:
+        pytest.skip(f"provider {provider.__name__} doesn isn't available")
+    provider.models.clear()
+    provider.instance = None
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,8 @@
 from pathlib import Path
-from typing import Iterator
 
 import pytest
 
-from llemon import (
-    LLM,
-    Anthropic,
-    DeepInfra,
-    Gemini,
-    LLMModel,
-    OpenAI,
-    enable_logs,
-    errors,
-)
+from llemon import enable_logs
 
 
 def pytest_configure(config):
@@ -22,22 +12,3 @@ def pytest_configure(config):
 @pytest.fixture
 def example_assets() -> Path:
     return Path(__file__).parent.parent / "examples" / "files"
-
-
-@pytest.fixture(
-    params=[
-        pytest.param((OpenAI, "gpt-5-nano"), id="gpt-5-nano"),
-        pytest.param((Anthropic, "claude-3-haiku-20240307"), id="claude-3-haiku-3"),
-        pytest.param((Gemini, "gemini-2.0-flash-lite"), id="gemini-2.0-flash-lite"),
-        pytest.param((DeepInfra, "meta-llama/Meta-Llama-3.1-8B-Instruct"), id="deepinfra-llama-3.1-8b"),
-    ]
-)
-def model(request: pytest.FixtureRequest) -> Iterator[LLMModel]:
-    provider: LLM = request.param[0]
-    model: str = request.param[1]
-    try:
-        yield provider.model(model)
-    except errors.ConfigurationError:
-        pytest.skip(f"provider {provider.__name__} doesn isn't available")
-    provider.models.clear()
-    provider.instance = None
