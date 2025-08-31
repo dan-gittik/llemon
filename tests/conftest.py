78f6f82805653433870a6fd6495dcf5647b636ef
gs202 <sheklerg@gmail.com>
2025-08-31 10:30:09 +0300

    changes to sync

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import pathlib
 import pytest
 
 from llemon import enable_logs, LLM
+from llemon.sync import LLM_Sync
 
 
 def pytest_configure(config: pytest.Config) -> None:
@@ -11,20 +12,20 @@ def pytest_configure(config: pytest.Config) -> None:
 
 
 @pytest.fixture
-def example_assets(llm: LLM) -> pathlib.Path:
+def example_assets(llm: LLM | LLM_Sync) -> pathlib.Path:
     """Provide a path to test assets that automatically skips tests for unsupported file types"""
 
     assets_base = pathlib.Path(__file__).parent.parent / "examples" / "files"
 
     class LLMCompatibleMimeType(type(assets_base)):
         def __truediv__(self, key):
-            result_path = super().__truediv__(key)
+            file_path = super().__truediv__(key)
 
-            if result_path.is_file():
-                mime_type, _ = mimetypes.guess_type(result_path)
+            if file_path.is_file():
+                mime_type, _ = mimetypes.guess_type(file_path)
                 if mime_type and llm.config.accepts_files and mime_type not in llm.config.accepts_files:
-                    pytest.skip(f"{llm} doesn't accept {mime_type} files (required for {result_path.name})")
+                    pytest.skip(f"{llm} doesn't accept {mime_type=} files (required for {file_path.name})")
 
-            return result_path
+            return file_path
 
     return LLMCompatibleMimeType(assets_base)
--- tests/sync/llm/test_files.py
+++ tests/sync/llm/test_files.py
@@ -1,35 +1,16 @@
 import pathlib
-from functools import wraps
-from typing import Any, Callable
 
 import pytest
 
 from llemon.sync import LLM
 
 
-def accepts_files(*mimetypes: str) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
-    def decorator(function: Callable[..., Any]) -> Callable[..., Any]:
-        @wraps(function)
-        def wrapper(llm: LLM, *args: Any, **kwargs: Any) -> Any:
-            accepted_files = llm.config.accepts_files or []
-            for mimetype in mimetypes:
-                if mimetype not in accepted_files:
-                    pytest.skip(f"{llm} doesn't accept {mimetype} files")
-            return function(llm, *args, **kwargs)
-
-        return wrapper
-
-    return decorator
-
-
-@accepts_files("image/png")
 def test_generate_with_file(llm: LLM, example_assets: pathlib.Path) -> None:
     my_file = example_assets / "hello.png"
     response = llm.generate("What is written in the file? Respond with the text only.", files=[my_file])
     assert response.text == "Hello, world!"
 
 
-@accepts_files("image/jpeg", "image/png")
 def test_generate_with_multiple_files(llm: LLM, example_assets: pathlib.Path) -> None:
     cat_file = example_assets / "cat.jpg"
     dog_file = example_assets / "dog.png"
